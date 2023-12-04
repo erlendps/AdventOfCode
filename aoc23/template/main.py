0e8939f2200111a6cf46db5f109c85a65214243b
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2023-12-04 11:45:39 +0100

    add template dir

---
+++ aoc23/template/main.py
@@ -0,0 +1,17 @@
+def get_lines(filename):
+    with open(filename, "r") as f:
+        lines = f.readlines()
+    return lines
+
+
+def task1(filename):
+    pass
+
+
+def task2(filename):
+    pass
+
+
+if __name__ == "__main__":
+    task1("test.txt")
+    task2("test.txt")
